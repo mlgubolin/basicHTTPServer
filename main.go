ce2e9766332ad48170c98000e28ad9c4c61e2589
Maria Lucia Gubolin <maria.gubolin@petrobras.com.br>
2025-10-31 15:54:42 -0300

    setup primeiro controlador

---
+++ main.go
@@ -0,0 +1,27 @@
+package simplehttpserver
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	SetRoutes(r)
+	http.ListenAndServe(":3333", r)
+}
